0edb1761d658ad03f921b99661756246ffbb248a
Tanmay Arora <aroratanmay34@gmail.com>
2024-05-28 13:12:06 +0530

    CharDataType

--- CharDataType/MyCharMain.java
+++ CharDataType/MyCharMain.java
@@ -1,8 +1,7 @@
 public class MyCharMain {
     public static void main(String[] args) {
         MyChar myChar = new MyChar('d');
-            // 'a','e','i','o','u' and their UpperCase
-        System.out.println(myChar.isVowel());
+        System.out.println(myChar.isVowel());     // 'a','e','i','o','u' and their UpperCase
         System.out.println(myChar.isNumber());
         MyChar.printLowerCaseAlphabets();
         MyChar.printUpperCaseAlphabets();
