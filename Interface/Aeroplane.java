66119cbed98b6f97b62f84cff6495e6f0f720eb6
Tanmay Arora <aroratanmay34@gmail.com>
2025-01-10 22:03:37 -0500

    Interface

---
+++ Interface/Aeroplane.java
@@ -0,0 +1,6 @@
+public class Aeroplane implements Flyable{
+    @Override
+    public void fly() {
+        System.out.println("Aeroplane is flying with fuel");
+    }
+}
---
+++ Interface/FlyableRunner.java
@@ -0,0 +1,8 @@
+public class FlyableRunner {
+    public static void main(String[] args) {
+        Flyable[] flyingObjects = {new Bird(), new Aeroplane()};
+        for (Flyable flyingObject : flyingObjects) {
+            flyingObject.fly();
+        }
+    }
+}
