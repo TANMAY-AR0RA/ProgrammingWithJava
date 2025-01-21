8e64be21822da14dae27e85c381589914dfa9367
Tanmay Arora <aroratanmay34@gmail.com>
2025-01-21 18:50:12 -0500

    oops

--- OOPS/CustomerRunner.java
+++ OOPS/CustomerRunner.java
@@ -1,5 +1,11 @@
 public class CustomerRunner {
     public static void main(String[] args) {
+
         Customer customer = new Customer();
+        customer.name = "John Doe";
+        customer.phoneNumber = "123-456-7890";
+
+        System.out.println("Customer Name: " + customer.name);
+        System.out.println("Customer Phone Number: " + customer.phoneNumber);
     }
 }
